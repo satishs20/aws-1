8e3f980b761adb479ba6eca297ab237a75437ffa
Jeriel Ng <ngjeriel59@gmail.com>
2022-09-23 13:01:36 +0800

    updated empApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -159,7 +159,7 @@ def AddEmp():
             db_conn.commit()
             full_name = "" + first_name2 + " " + last_name2
             # Upload image file in S3 #
-            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id2) + "_image_file"
+            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id2) + "_image_file" + str(".png")
             s3 = boto3.resource('s3')
 
             try:
